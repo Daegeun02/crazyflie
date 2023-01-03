36af2349fb1a8216e873dfd33eb0f9276cfba551
daegeun02 <87759632+Daegeun02@users.noreply.github.com>
2023-01-03 15:21:03 +0900

    Add files via upload

---
+++ imu.py
@@ -0,0 +1,63 @@
+## sensoring
+from cflib.crazyflie.log import LogConfig
+
+period_in_ms = 10
+
+class IMU:
+
+    def __init__(self, scf):
+        # self.state = state
+        self.scf = scf
+        self.cf  = scf.cf
+
+
+    def start_get_velocity(self, period_in_ms=period_in_ms):
+        log_conf = LogConfig(name="velocity", period_in_ms=period_in_ms)
+        log_conf.add_variable('stateEstimate.vx', 'FP16')
+        log_conf.add_variable('stateEstimate.vy', 'FP16')
+        log_conf.add_variable('stateEstimate.vz', 'FP16')
+
+        self.scf.cf.log.add_config(log_conf)
+        log_conf.data_received_cb.add_callback(self.velocity_callback)
+        log_conf.start()
+ 
+
+    def start_get_acc(self, period_in_ms=period_in_ms):
+        log_conf = LogConfig(name='acceleration', period_in_ms=period_in_ms)
+        log_conf.add_variable('acc.x', 'FP16')      ## m/s^2
+        log_conf.add_variable('acc.y', 'FP16')      ## m/s^2
+        log_conf.add_variable('acc.z', 'FP16')      ## m/s^2
+
+        self.scf.cf.log.add_config(log_conf)
+        log_conf.data_received_cb.add_callback(self.accelerate_callback)
+        log_conf.start()
+
+
+    def start_get_euler_vel(self, period_in_ms=period_in_ms):
+        log_conf = LogConfig(name='Euler_angle_velocity', period_in_ms=period_in_ms)
+        log_conf.add_variable('gyro.x', 'FP16')     ## deg/s
+        log_conf.add_variable('gyro.y', 'FP16')     ## deg/s
+        log_conf.add_variable('gyro.z', 'FP16')     ## deg/s
+
+        self.scf.cf.log.add_config(log_conf)
+        log_conf.data_received_cb.add_callback(self.eulervel_callback)
+        log_conf.start()
+
+    
+    ### callbacks ### 
+    def velocity_callback(self, timestamp, data, logconf):
+        self.cf.vel[0] = data['stateEstimate.vx']
+        self.cf.vel[1] = data['stateEstimate.vy']
+        self.cf.vel[2] = data['stateEstimate.vz']
+
+
+    def accelerate_callback(self, timestamp, data, logconf):
+        self.cf.acc[0] = data['acc.x'] * 9.81
+        self.cf.acc[1] = data['acc.y'] * 9.81
+        self.cf.acc[2] = data['acc.z'] * 9.81
+
+    
+    def eulervel_callback(self, timestamp, data, logconf):
+        self.cf.euler_vel[0] = data['gyro.x']
+        self.cf.euler_vel[1] = data['gyro.y']
+        self.cf.euler_vel[2] = data['gyro.z']
---
+++ qualisys_estimator.py
@@ -0,0 +1,105 @@
+from threading import Thread
+
+from scipy.spatial.transform import Rotation
+
+import xml.etree.cElementTree as ET
+
+import qtm
+import asyncio
+
+from math import isnan
+
+send_full_pose = True
+
+
+
+class QtmWrapper(Thread):
+    def __init__(self, body_name):
+        Thread.__init__(self)
+
+        self.body_name = body_name
+        self.on_pose = None
+        self.connection = None
+        self.qtm_6DoF_labels = []
+        self._stay_open = True
+
+        self.start()
+
+    def close(self):
+        self._stay_open = False
+        self.join()
+
+    def run(self):
+        asyncio.run(self._life_cycle())
+
+    async def _life_cycle(self):
+        await self._connect()
+        while (self._stay_open):
+            await asyncio.sleep(1)
+        await self._close()
+
+    async def _connect(self):
+
+        self.connection = await qtm.connect('127.0.0.1')
+        
+        if self.connection is None:
+            print("Failed to connect")
+            return
+
+        params = await self.connection.get_parameters(parameters=['6d'])
+        self.params = params
+        xml = ET.fromstring(params)
+        self.qtm_6DoF_labels = [label.text.strip() for index, label in enumerate(xml.findall('*/Body/Name'))]
+
+        await self.connection.stream_frames(
+            components=['6deuler'],
+            on_packet=self._on_packet)
+
+    async def _discover(self):
+        async for qtm_instance in qtm.Discover('192.168.254.1'):
+            return qtm_instance
+
+    def _on_packet(self, packet):
+        header, bodies = packet.get_6d_euler()
+
+        if bodies is None:
+            return
+
+        if self.body_name not in self.qtm_6DoF_labels:
+            print('Body ' + self.body_name + ' not found.')
+        else:
+            ## who i am
+            index = self.qtm_6DoF_labels.index(self.body_name)
+            ## where i am and which orientation i have
+            temp_cf_data = bodies[index]
+
+            position = temp_cf_data[0]
+            x = position[0]                     ## m
+            y = position[1]                     ## m
+            z = position[2]                     ## m
+
+            euler = temp_cf_data[1]
+            R = euler[2]                        ## deg
+            P = euler[1]                        ## deg
+            Y = euler[0]                        ## deg
+
+            if self.on_pose:
+                # Make sure we got a position
+                if isnan(x):
+                    return
+
+                self.on_pose([x, y, z, R, P, Y])
+
+    async def _close(self):
+        await self.connection.stream_frames_stop()
+        self.connection.disconnect()
+
+
+def send_pose(cf, x, y, z, R, P, Y):
+    cf.pos[0] = x
+    cf.pos[1] = y
+    cf.pos[2] = z
+
+    cf.euler_pos[0] = R
+    cf.euler_pos[1] = P
+    cf.euler_pos[2] = Y
