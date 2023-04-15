7ca780169a36e9aae444cbe2637bba386b22012a
daegeun02 <redhawkdg02@gmail.com>
2023-04-16 07:01:42 +0900

    finished implement swarm commander

--- swarm/swarm_commander.py
+++ swarm/swarm_commander.py
@@ -1,44 +1,76 @@
+from threading import Thread
+
 from cflib.crazyflie.swarm import Swarm
 
-from ..controller import Commander
+from controller import Commander
 
-from .swarm_loops import _acc_command_loop
-from .swarm_loops import _vel_command_loop
+from sensor import start
+from sensor import QtmWrapper
 
-
-
-def _swarm_commander(scfs: dict, dt=0.1):
-    commanders = {}
-
-    for uri, scf in scfs.items():
-        commanders[uri] = scf, Commander(scf.cf, dt)
-
-    return commanders
+from time import sleep
 
 
-class SwarmCommander(Swarm):
 
-    def spmc_by_acc_start(self):
-        """
-        spmc: Single Processor Multi Command 
-        """
-        self.parallel_safe(
-            _acc_command_loop, 
-            args_dict=_swarm_commander(self._cfs)
-        )
+class SwarmCommander(Swarm, Thread):
+
+
+    def __init__(self, uris, dt):
+
+        super().__init__( uris )
+
+        self.dt = dt
+
+        self.thread_is_alive = False
+
+        self.qtm_wrappers = []
 
     
-    def spmc_by_vel_start(self):
-        """
-        spmc: Single Processor Multi Command 
-        """
-        self.parallel_safe(
-            _vel_command_loop,
-            args_dict=_swarm_commander(self._cfs)
-        )
-
-    
-    def upload_acc_cmd(self, acccmds: dict):
+    def start_sensor(self, rgbn):
 
         for uri, scf in self._cfs.items():
-            scf.cf.command[:] = acccmds[uri]
+
+            qtm_wrapper = QtmWrapper( body_name=rgbn[uri] )
+
+            self.qtm_wrappers.append( qtm_wrapper )
+
+            start( scf, qtm_wrapper )
+
+        sleep( 1 )
+
+
+    def run(self):
+
+        threads = []
+        reporter = self.Reporter()
+
+        self.thread_is_alive = True
+
+        ## spawn commander
+        for uri, scf in self._cfs.items():
+            
+            thread = Commander( scf, self.dt )
+            threads.append( thread )
+            thread.start()
+        
+        ## go go
+        for thread in threads:
+            thread.init_send_setpoint()
+
+        ## monitoring
+        while self.thread_is_alive:
+
+            if reporter.is_error_reported():
+                error = reporter.errors[0]
+                raise Exception( ' Error ' ) from error
+
+        ## end.. Let's go to home
+        for thread in threads:
+            thread.stop_send_setpoint()
+
+
+    def stop_all(self):
+
+        self.thread_is_alive = False
+
+        for qtm_wrapper in self.qtm_wrappers:
+            qtm_wrapper.close()
